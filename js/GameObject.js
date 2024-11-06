d7cb09d5f31e9ec0d4cf5ff290c80928df1ba716
mattxix <marussas@email.neit.edu>
2024-11-06 13:39:20 -0500

    imported base files

---
+++ js/GameObject.js
@@ -0,0 +1,90 @@
+class GameObject
+{
+    constructor()
+    {
+    /*Object properties (or attributes)*/
+    //object position
+     this.x=c.width/2;
+     this.y=c.height/2;
+     //object angle
+     this.angle = 0;
+     //object dimensions
+     this.w=100;
+     this.h=100;
+     //object velocity
+     this.vx=0;
+     this.vy=0;
+    //object color
+     this.color = `hotpink`  
+
+     
+    }
+
+    //Draws a rectangle 
+    render()
+    {
+        ctx.save();
+            ctx.fillStyle = this.color
+            ctx.translate(this.x, this.y)
+            ctx.rotate(this.angle*Math.PI/180)
+            ctx.fillRect(-this.w/2, -this.h/2, this.w, this.h)
+        ctx.restore();
+    }
+
+    //Moves an object by adding it's velocity to it's position on each axis
+    move()
+    {
+        this.x = this.x + this.vx
+        this.y = this.y + this.vy
+    }
+
+    //Each function below returns a sides of this object's bounding box
+    top()
+    {
+        return {x:this.x, y:this.y - this.h/2};
+    }
+    bottom()
+    {
+        return {x:this.x, y:this.y + this.h/2};
+    }
+    left()
+    {
+        return {x:this.x - this.w/2, y:this.y};
+    }
+    right()
+    {
+        return {x:this.x + this.w/2, y:this.y};
+    }
+
+    /*-------Collision Function ----------------
+    Used to check for collision between 2 objects
+    This method checks to see where the various sides of one object are in relationship to another object's sides
+    -------------------------------------------*/
+    overlaps(_obj)
+    {
+        if(
+            this.top().y < _obj.bottom().y &&
+            this.bottom().y > _obj.top().y &&
+            this.left().x < _obj.right().x &&
+            this.right().x > _obj.left().x
+        )
+        {
+            
+            return true
+        }
+        return false;
+    }
+    isOverPoint(_point)
+    {
+        if(
+            this.top().y < _point.y &&
+            this.bottom().y > _point.y &&
+            this.left().x < _point.x &&
+            this.right().x > _point.x
+        )
+        {
+            return true
+        }
+        return false; 
+    }
+}
---
+++ js/game.js
@@ -0,0 +1,77 @@
+/*-------------------------------------------
+Game Setup
+ 1. canvas 
+ 2. context
+ 3. frame rate
+ 4. animation timer runs main function 60 frames per second
+-------------------------------------------*/
+var c = document.querySelector(`canvas`)
+var ctx = c.getContext(`2d`)
+var fps = 1000/60
+var timer = setInterval(main, fps)
+
+
+/*------------Declare Variables Here--------*/
+
+
+
+
+/*--------------main()------------------------
+This is the function that makes the game work
+---------------------------------------------*/
+
+function main()
+{
+    //erases the screen
+    ctx.clearRect(0,0,c.width,c.height); 
+
+    //Any changes to numbers
+
+    //Any collision detection 
+
+    //draw the pictures
+}
+
+//random number generator
+function rand(_low, _high)
+{
+    return Math.random()*(_high - _low) + _low;
+}
+//Converts degrees to radians
+function radians(_deg)
+{
+    return _deg * Math.PI/180
+}
+
+//Converts radians to degrees
+function degrees(_rad)
+{
+    return _rad * 180/Math.PI
+}
+/*-------Diagram--------
+
+               /|        c = the hypoteneuse
+            c / |        b = height
+             /  | b      a = width
+            /   |        T = arch tangent angle
+           /T___|
+             a
+
+--------------------------
+
+To get a and b (displacement) when you know two points
+  
+    a = destination.x - starting.x
+    b = destination.y - starting.y
+
+To get the total distance (hypotenuese) between two points
+    c = Math.sqrt(_a*_a + _b*_b)
+
+To get the arc tangent angle (labeled T in the diagram)
+    radians = Math.atan2(b, a)
+
+To find a and b if you know c and T
+    a = Math.cos(T) * c
+    b = Math.sin(T) * c
+
+*/
